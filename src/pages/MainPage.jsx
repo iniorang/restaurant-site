0f3fb6ba1d22f8bae6df02c9e0cfcb4720951c58
Muhammad Faqih <45988454+iniorang@users.noreply.github.com>
2026-03-08 01:17:14 +0700

    Renaming picture

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,10 +3,22 @@ import React from "react"
 export default function MainPage() {
     return (
         <main>
-            <section className="bg-[url(/assets/img/Front_Page/NasiCampurBali.png)] bg-cover bg-center bg-no-repeat w-full h-screen flex items-center justify-center bg-black/50 bg-blend-darken" id="hero-section">
-                <h1 className="text-white text-5xl font-bold z-10">
-                    Welcome to Warung Hati
-                </h1>
+            <section className="bg-[url('/assets/img/Utama/NasiCampurBali2.png')] bg-cover bg-center bg-no-repeat w-full h-screen flex bg-black/50 bg-blend-darken" id="hero-section">
+                <div className="flex items-center justify-between px-10">
+                    <div>
+                        <h1 className="text-white text-9xl">
+                            Lorem ipsum dolor sit
+                        </h1>
+                        <p className="text-white z-10">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus.
+                        </p>
+                    </div>
+                    <div className="bg-white/50 p-5 rounded-lg ml-10">
+                        <p>test</p>
+                        <p>test</p>
+                        <p>test</p>
+                    </div>
+                </div>
             </section>
         </main>
     )
